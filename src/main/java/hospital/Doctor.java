aae83708e0b8730364480927a305abb62d97e67f
Adam Mężydło <adamkmezydlo@gmail.com>
2024-06-02 13:08:00 +0200

    doctor <-> technician working

--- src/main/java/hospital/Doctor.java
+++ src/main/java/hospital/Doctor.java
@@ -3,10 +3,13 @@ package hospital;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
 
 import static hospital.ExaminationType.*;
 
@@ -15,62 +18,145 @@ public class Doctor {
     private final static String EXAMINATION_EXCHANGE = "examination";
     private final static String RESULTS_EXCHANGE = "results";
 
+    private final Channel channel;
+    private String doctorName;
+    private final Scanner sc;
 
-    public static void main(String[] argv) throws Exception {
+    private String resultsQueue;
+
+    public Doctor() throws IOException, TimeoutException {
+        channel = createChannel();
+        declareExchanges();
+
+        sc = new Scanner(System.in);
+    }
+
+    private Channel createChannel() throws IOException, TimeoutException {
+        final Channel channel;
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
 
-        try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel();
-             Scanner sc = new Scanner(System.in)) {
-            channel.exchangeDeclare(EXAMINATION_EXCHANGE, "topic");
+        Connection connection = factory.newConnection();
+        channel = connection.createChannel();
+        return channel;
+    }
+
+    private void declareExchanges() throws IOException {
+        channel.exchangeDeclare(EXAMINATION_EXCHANGE, "topic");
+        channel.exchangeDeclare(RESULTS_EXCHANGE, "direct");
+    }
 
 
-            while (sc.hasNextLine()) {
-                String examinationJob = sc.nextLine();
-                String[] examinationArgs = examinationJob.split(" ");
+    private void declareQueues() throws IOException {
+        resultsQueue = channel.queueDeclare(
+                doctorName,
+                false,
+                false,
+                false,
+                null
+        ).getQueue();
+    }
 
-                if (examinationArgs.length != 2) {
-                    System.out.println("Unknown command");
-                    continue;
-                }
+    private void bindQueues() throws IOException {
+        channel.queueBind(resultsQueue, RESULTS_EXCHANGE, doctorName);
+    }
 
-                String examinationTypeStr = examinationArgs[0];
-                String patientName = examinationArgs[1];
-
-                ExaminationType examinationType;
-                try {
-                    examinationType = ExaminationType.valueOf(examinationTypeStr.toUpperCase());
-                } catch (IllegalArgumentException e) {
-                    System.out.println("Invalid examination type: " + examinationTypeStr);
-                    continue;
-                }
-
-                orderExamination(examinationType, channel, patientName);
-
-
-            }
-
-
+    private String readDoctorName() throws IOException {
+        System.out.print("Enter doctor name: ");
+        if (sc.hasNextLine()) {
+            return sc.nextLine().trim().toLowerCase();
+        } else {
+            throw new IOException("Doctor name is a must");
         }
     }
 
-    private static void orderExamination(ExaminationType examinationType, Channel channel, String patientName) throws IOException {
+    private DeliverCallback setResultsHandler() {
+
+        return (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            try {
+                handleResults(message);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+
+        };
+
+    }
+
+    private void listenQueues(DeliverCallback deliverCallback) throws IOException {
+        channel.basicConsume(resultsQueue, true, deliverCallback, consumerTag -> {
+        });
+    }
+
+    private void handleResults(String message) throws InterruptedException {
+        Random rand = new Random();
+        System.out.println("Got " + message);
+        // simulate heavy task
+        Thread.sleep(rand.nextInt(1, 10) * 1000L);
+
+        System.out.println(doctorName + " Done");
+    }
+
+    private void run() throws IOException {
+        doctorName = readDoctorName();
+        declareQueues();
+        bindQueues();
+
+        DeliverCallback deliverCallback = setResultsHandler();
+        listenQueues(deliverCallback);
+
+
+        // main loop
+        while (sc.hasNextLine()) {
+            String examinationJob = sc.nextLine();
+            String[] examinationArgs = examinationJob.split(" ");
+
+            if (examinationArgs.length != 2) {
+                System.out.println("Unknown command");
+                continue;
+            }
+
+            String examinationTypeStr = examinationArgs[0];
+            String patientName = examinationArgs[1];
+
+            ExaminationType examinationType;
+            try {
+                examinationType = ExaminationType.valueOf(examinationTypeStr.toUpperCase());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid examination type: " + examinationTypeStr);
+                continue;
+            }
+
+            String order = doctorName + " " + patientName;
+            orderExamination(examinationType, channel, order);
+
+
+        }
+
+        sc.close();
+    }
+
+
+    private void orderExamination(ExaminationType examinationType, Channel channel, String message) throws IOException {
+
+        System.out.println("[" + doctorName + "] " + message);
         switch (examinationType) {
             case KNEE:
+
                 channel.basicPublish(
-                        EXAMINATION_EXCHANGE, 
+                        EXAMINATION_EXCHANGE,
                         KNEE.name(),
-                        null, 
-                        patientName.getBytes(StandardCharsets.UTF_8)
+                        null,
+                        message.getBytes(StandardCharsets.UTF_8)
                 );
                 break;
             case HIP:
                 channel.basicPublish(
                         EXAMINATION_EXCHANGE,
                         HIP.name(),
-                        null, 
-                        patientName.getBytes(StandardCharsets.UTF_8)
+                        null,
+                        message.getBytes(StandardCharsets.UTF_8)
                 );
                 break;
             case ELBOW:
@@ -78,10 +164,16 @@ public class Doctor {
                         EXAMINATION_EXCHANGE,
                         ELBOW.name(),
                         null,
-                        patientName.getBytes(StandardCharsets.UTF_8)
+                        message.getBytes(StandardCharsets.UTF_8)
                 );
                 break;
         }
+    }
+
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Doctor doctor = new Doctor();
+        doctor.run();
     }
 
 
--- src/main/java/hospital/Technician.java
+++ src/main/java/hospital/Technician.java
@@ -1,35 +1,69 @@
 package hospital;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.*;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
 
 public class Technician {
     private final static String EXAMINATION_EXCHANGE = "examination";
+    private final static String RESULTS_EXCHANGE = "results";
 
+    private final Channel channel;
+    private ExaminationType examinationOne;
+    private ExaminationType examinationTwo;
+    private String exQueueOne;
+    private String exQueueTwo;
 
+    public Technician() throws Exception {
+        channel = createChannel();
+        declareExchanges();
+    }
 
-
-    public static void main(String[] args) throws Exception{
+    private Channel createChannel() throws IOException, TimeoutException {
+        final Channel channel;
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
 
         Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
+        channel = connection.createChannel();
+        return channel;
+    }
 
-        channel.exchangeDeclare(EXAMINATION_EXCHANGE,"topic");
+    private void declareExchanges() throws IOException {
+        channel.exchangeDeclare(EXAMINATION_EXCHANGE, "topic");
+        channel.exchangeDeclare(RESULTS_EXCHANGE, "direct");
 
+    }
 
+    private void declareQueues() throws IOException {
+        exQueueOne = channel.queueDeclare(
+                examinationOne.name(),
+                false,
+                false,
+                false,
+                null
+        ).getQueue();
+        exQueueTwo = channel.queueDeclare(
+                examinationTwo.name(),
+                false,
+                false,
+                false,
+                null
+        ).getQueue();
+    }
 
+    private void bindQueues() throws IOException {
+        channel.queueBind(exQueueOne, EXAMINATION_EXCHANGE, examinationOne.toString());
+        channel.queueBind(exQueueTwo, EXAMINATION_EXCHANGE, examinationTwo.toString());
 
-        ExaminationType examinationOne;
-        ExaminationType examinationTwo;
+    }
 
-        try(Scanner sc = new Scanner(System.in)){
+    private void setAvailableExaminationTypes() {
+        try (Scanner sc = new Scanner(System.in)) {
             String line = sc.nextLine();
             String[] availableExaminationTypes = line.split(" ");
 
@@ -43,39 +77,63 @@ public class Technician {
                 examinationTwo = ExaminationType.valueOf(availableExaminationTypes[1].toUpperCase());
             } catch (IllegalArgumentException e) {
                 System.out.println("Invalid examination type: " + availableExaminationTypes[0] + " or " + availableExaminationTypes[1]);
-                return;
             }
 
         }
+    }
 
-        String exQueueOne = channel.queueDeclare(
-                examinationOne.name(),
-                false,
-                false,
-                false,
-                null
-        ).getQueue();
-        String exQueueTwo = channel.queueDeclare(
-                examinationTwo.name(),
-                false,
-                false,
-                false,
-                null
-        ).getQueue();
+    private  void handleExamination(String message, Delivery delivery) throws InterruptedException, IOException {
+        Random rand = new Random();
+        String[] messageParts = message.split(" ");
 
-        System.out.println(exQueueOne + " " + exQueueTwo);
+        System.out.println(message);
+        Thread.sleep(rand.nextInt(1, 10) * 1000L);
 
-        channel.queueBind(exQueueOne,EXAMINATION_EXCHANGE,examinationOne.toString());
-        channel.queueBind(exQueueTwo,EXAMINATION_EXCHANGE,examinationTwo.toString());
+        System.out.println("DONE");
+        String replyMessage = "Done";
+        // TODO autoAck off
 
+        channel.basicPublish(RESULTS_EXCHANGE, messageParts[0], null, replyMessage.getBytes(StandardCharsets.UTF_8));
+    }
 
-        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+    private DeliverCallback setExaminationHandler() {
+
+        return (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-            System.out.println("Received: " + delivery.getEnvelope().getRoutingKey() + ": " + message);
+            try {
+                handleExamination(message, delivery);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+
         };
 
-        channel.basicConsume(exQueueOne,true,deliverCallback,consumerTag -> {});
-        channel.basicConsume(exQueueTwo,true,deliverCallback,consumerTag -> {});
+    }
+
+    private void listenQueues(DeliverCallback deliverCallback) throws IOException {
+        // nasłuchuj na 2 kolejkach związanych z tym, czego leczysz
+        channel.basicConsume(exQueueOne, true, deliverCallback, consumerTag -> {
+        });
+        channel.basicConsume(exQueueTwo, true, deliverCallback, consumerTag -> {
+        });
+    }
+
+
+    public void run() throws Exception {
+        setAvailableExaminationTypes();
+        declareQueues();
+        bindQueues();
+
+        DeliverCallback deliverCallback = setExaminationHandler();
+        listenQueues(deliverCallback);
 
     }
+
+
+    public static void main(String[] args) throws Exception {
+        Technician technician = new Technician();
+        technician.run();
+    }
+
+
 }
