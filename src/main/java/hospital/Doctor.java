eb65e93a12a89c9ce0cddba165e0c4bfadcec5c3
Adam Mężydło <adamkmezydlo@gmail.com>
2024-06-02 02:17:39 +0200

    Create Doctor

---
+++ src/main/java/hospital/Doctor.java
@@ -0,0 +1,91 @@
+package hospital;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+enum ExaminationType {
+    KNEE,
+    HIP,
+    ELBOW
+}
+
+public class Doctor {
+    private final static String EXAMINATION_EXCHANGE = "examination";
+    private final static String RESULTS_EXCHANGE = "results";
+
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel();
+             Scanner sc = new Scanner(System.in)) {
+            channel.exchangeDeclare(EXAMINATION_EXCHANGE, "topic");
+
+
+            while (sc.hasNextLine()) {
+                String examinationJob = sc.nextLine();
+                String[] examinationArgs = examinationJob.split(" ");
+
+                if (examinationArgs.length != 2) {
+                    System.out.println("Unknown command");
+                    continue;
+                }
+
+                String examinationTypeStr = examinationArgs[0];
+                String patientName = examinationArgs[1];
+
+                ExaminationType examinationType;
+                try {
+                    examinationType = ExaminationType.valueOf(examinationTypeStr.toUpperCase());
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Invalid examination type: " + examinationTypeStr);
+                    continue;
+                }
+
+                orderExamination(examinationType, channel, patientName);
+
+
+            }
+
+
+        }
+    }
+
+    private static void orderExamination(ExaminationType examinationType, Channel channel, String patientName) throws IOException {
+        switch (examinationType) {
+            case KNEE:
+                channel.basicPublish(
+                        EXAMINATION_EXCHANGE, 
+                        ExaminationType.KNEE.name(), 
+                        null, 
+                        patientName.getBytes(StandardCharsets.UTF_8)
+                );
+                break;
+            case HIP:
+                channel.basicPublish(
+                        EXAMINATION_EXCHANGE,
+                        ExaminationType.HIP.name(), 
+                        null, 
+                        patientName.getBytes(StandardCharsets.UTF_8)
+                );
+                break;
+            case ELBOW:
+                channel.basicPublish(
+                        EXAMINATION_EXCHANGE,
+                        ExaminationType.ELBOW.name(),
+                        null,
+                        patientName.getBytes(StandardCharsets.UTF_8)
+                );
+                break;
+        }
+    }
+
+
+}
